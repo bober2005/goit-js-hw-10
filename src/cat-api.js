a4b385bed2c4b7f78329181bf85ed18ea80960df
bober2005 <yaroslavd1290@gmail.com>
2023-08-28 13:48:35 +0300

    8

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,23 +1,22 @@
-const URL = 'https://api.thecatapi.com/v1';
-const API_KEY =
-    'live_C9iEtPg54awJpTo4QX7RoGFsgMBvEc90dWA73rvw3AuZJatT06iGrkqCDbqdNZmr';
+const url = 'https://api.thecatapi.com/v1';
+const api_key = "live_i0tDaOGInqQ26rh7JUwZgUxN2ia9tgGfeQAmLlGg5e4sjZuijJKVoZO121TcrnQ9";
 
 export function fetchBreeds() {
-    return fetch(`${URL}/breeds?api_key=${API_KEY}`).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    });
-}
+    return fetch(`${url}/breeds?api_key=${api_key}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
+};
 
 export function fetchCatByBreed(breedId) {
-    return fetch(
-        `${URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
-    ).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    });
-}
+    return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
+};
